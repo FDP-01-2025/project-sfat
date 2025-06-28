e6efbb134eceec96f2041e024cdddd401eb61523
Emerson Mauricio Surio Méndez <00247025@uca.edu.sv>
2025-06-28 00:01:11 -0600

    Add files via upload
    Subi los primeros avances realizados

---
+++ scr/levels.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+void level_01(){
+    int decision;
+    int plataforma;
+    bool exito;
+
+    int caminoCorrecto[5] = {2, 5, 3, 1, 4};
+
+    cout << "== MINIJUEGO #1: Cruza el las 5 plataformas ==" << endl;
+    cout << "Debes elegir la opcion correcta en cada plataforma para avanzar." << endl;
+
+    do {
+        plataforma = 1;
+        exito = true;
+
+        while (plataforma <= 5 && exito) {
+            cout << "Elige una opcion (1, 2, 3, 4 o 5): "<<endl;
+            cout << "Plataforma " << plataforma << endl;
+            cin >> decision;
+
+            if (decision == caminoCorrecto[plataforma - 1]) {
+                cout << "Correcto! Avanzas a la siguiente plataforma." << endl;
+                plataforma++;
+            } else {
+                cout << "Incorrecto! Has caido." << endl;
+                exito = false;
+            }
+        }
+
+    } while (!exito);
+
+    cout << "Felicidades! Has cruzado todas las plataformas." << endl;
+}
