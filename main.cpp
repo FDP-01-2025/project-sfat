5f1a6be8f2fffef6a347ab458cd7fe6cec4647ff
Emerson Mauricio Surio Méndez <00247025@uca.edu.sv>
2025-07-05 01:09:04 -0600

    Implementación de Inicio de Sesión y Menús

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <fstream>
-#include "scr/header.h"
+#include "scr/levels.h"
+#include "scr/user.h"
+#include "scr/menu.h"
 using namespace std;
 
-int main(){
-    int opcion;
+int main() {
+    int option;
+    bool logged_In = false;
 
-    cout << "Que nivel quiere jugar? ";
-    cin >> opcion;
+    do {
+        cout << "\n=== Menú de Usuario ===\n";
+        cout << "1. Registrarse\n";
+        cout << "2. Iniciar Sesión\n";
+        cout << "3. Salir\n";
+        cout << "Seleccione una opción: ";
+        cin >> option;
 
-    if (opcion == 1) level_01();
-    
+        switch (option) {
+            case 1:
+                SignUp();
+                break;
+            case 2:
+                logged_In = logIn();
+                if (logged_In) mainMenu();
+                break;
+            case 3:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opción inválida.\n";
+        }
+        
+    } while (option != 3);
+
+    return 0;
 }
