06a1cef09928c6b5b5c1ba037086364052ddf43b
Emerson Mauricio Surio Méndez <00247025@uca.edu.sv>
2025-07-05 03:00:50 -0600

    Add files via upload

---
+++ scr/gameplay.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "gameplay.h"
+#include "levels.h"
+using namespace std;
+
+void gameplay(){
+
+    level_01();
+    nextLevel_Message();
+}
---
+++ scr/gameplay.h
@@ -0,0 +1,11 @@
+#ifndef GAMEPLAY_H
+#define GAMEPLAY_H
+
+void gameTitle();
+
+void gameplay();
+void nextLevel_Message();
+
+void gameOver_Message();
+
+#endif
