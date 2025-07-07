1f39cf419253a1221ebc424121e8d44b626f71e1
Emerson Mauricio Surio Méndez <00247025@uca.edu.sv>
2025-07-07 13:58:23 -0600

    FUNCIONES MENUS *TERMINADO*

--- scr/menu.cpp
+++ scr/menu.cpp
@@ -3,12 +3,54 @@
 #include "menu.h"
 #include "gameplay.h"
 #include "levels.h"
+#include "user.h"
 using namespace std;
+
+void userMenu() {
+    int option;
+    bool logged_In = false;
+
+    do {
+
+        cout << endl;
+        cout << "==============================" << endl;
+        cout << "        MENU DE USUARIO       " << endl;
+        cout << "==============================" << endl;
+        cout << "1. Registrarse" << endl;
+        cout << "2. Iniciar Sesion" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Seleccione una opcion: ";
+
+        while (!(cin >> option)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Entrada invalida. Por favor, ingrese un numero: ";
+        }
+
+        switch (option) {
+            case 1:
+                SignUp();
+                break;
+            case 2:
+                logged_In = logIn();
+                if (logged_In) mainMenu();
+                break;
+            case 3:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opcion invalida" << endl;
+        }
+
+    } while (option != 3);
+
+}
 
 void mainMenu() {
     int option;
     do {
         
+        system("cls");
         gameTitle();
 
         cout << "\n==========================================" << endl;
@@ -43,3 +85,4 @@ void mainMenu() {
     } while (option != 2);
     
 }
+
